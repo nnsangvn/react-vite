1567eb77776935224b32b82f28dbbce0d9ae9106
Nguyễn Nhất Sang <nguyennhatsang2002@gmail.com>
2025-03-21 23:21:41 +0700

    video 30 JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,14 @@
+// JSX
+// fragment
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div> react component</div>
+        //fragment
+        <>
+            <div> react component</div>
+            <div className='child' style={{ borderRadius: "10px" }}>child</div>
+        </>
     );
 }
 
