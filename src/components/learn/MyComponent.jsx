2b8d25cb58f00fe3a9b89e0647609507f2b2d804
Nguyễn Nhất Sang <nguyennhatsang2002@gmail.com>
2025-03-21 23:53:26 +0700

    video 31 sử dụng biến số với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,24 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const it = "1"; // string
+    // const it = 22.2; // number
+
+    //=> Khong tin ra giá trị
+    // const it = true; //boolean
+    // const it = undefined;
+    // const it = null;
+
+    // const it = [1, 2, 3];
+    const it = {
+        name: "child",
+        age: 23
+    }
     return (
         //fragment
         <>
-            <div> react component</div>
+            <div> {JSON.stringify(it)} & react </div>
+            <div>{console.log("Sang")}</div>
             <div className='child' style={{ borderRadius: "10px" }}>child</div>
         </>
     );
