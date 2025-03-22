227ac8b32102d02656fba92c1054e80109fa8d43
Nguyễn Nhất Sang <nguyennhatsang2002@gmail.com>
2025-03-22 10:48:43 +0700

    video 30 chia component

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+const TodoData = () => {
+    return (
+
+        <div className='todo-data'>
+            <div>Learn React</div>
+            <div>Learn React</div>
+        </div>
+    );
+}
+
+export default TodoData;
